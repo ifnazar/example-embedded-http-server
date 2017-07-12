677adef26282d87937bded9f390c9efea74321ec
Igor Fabiano Nazar <nazar@objective.com.br>
2017-07-11 21:23:10 -0300

    standardized

--- jetty-jar/src/main/java/nif/example/HelloServelet.java
+++ jetty-jar/src/main/java/nif/example/HelloServelet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class MavenJettyInt extends HttpServlet {
+public class HelloServelet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 
